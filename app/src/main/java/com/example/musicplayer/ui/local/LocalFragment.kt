aae897e3d1cefd3df92f2e671d561a5e07b4acfe
moke <2061610579@qq.com>
2024-05-12 00:44:31 +0800

    init

---
+++ app/src/main/java/com/example/musicplayer/ui/local/LocalFragment.kt
@@ -0,0 +1,42 @@
+package com.example.musicplayer.ui.local
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.musicplayer.databinding.FragmentLocalBinding
+
+class LocalFragment : Fragment() {
+
+    private var _binding: FragmentLocalBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val dashboardViewModel =
+            ViewModelProvider(this).get(DashboardViewModel::class.java)
+
+        _binding = FragmentLocalBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textLocal
+        dashboardViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
